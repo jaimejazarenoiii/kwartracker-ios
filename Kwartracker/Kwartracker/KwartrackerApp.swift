b463e850bfcbd362f7a10aec204b9589691ad062
Jacob <jacob_potolin@ligph.com>
2021-03-25 20:30:11 +0800

    Kwartracker project

---
+++ Kwartracker/Kwartracker/KwartrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KwartrackerApp.swift
+//  Kwartracker
+//
+//  Created by Jacob on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct KwartrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
