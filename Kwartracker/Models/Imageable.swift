e89c9b18d21a8abb1d904ca30c5c1e9ad4e5d0f3
Jacob <jacob_potolin@ligph.com>
2021-04-25 14:13:12 +0800

    Add new models Imageable, Transaction and Wallet

---
+++ Kwartracker/Models/Imageable.swift
@@ -0,0 +1,13 @@
+//
+//  Imageable.swift
+//  Kwartracker
+//
+//  Created by Jacob on 4/25/21.
+//
+
+import UIKit
+
+struct Imageable {
+    var rawURL: String = ""
+    var image: UIImage? = nil
+}
