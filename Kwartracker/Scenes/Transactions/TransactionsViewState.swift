cd9369870f5e32cd128804a7ebfa4be83ffa4a35
Jacob <jacob_potolin@ligph.com>
2021-04-26 08:53:47 +0800

    Add transaction reducer, state, and action

---
+++ Kwartracker/Scenes/Transactions/TransactionsViewState.swift
@@ -0,0 +1,72 @@
+//
+//  TransactionsViewState.swift
+//  Kwartracker
+//
+//  Created by Jacob on 4/26/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct TransactionsViewState {
+    var transactions: [Transaction] = sampleTransactions
+    var transaction: Transaction? = nil
+}
+
+func transactionsViewReducer(
+    state: inout TransactionsViewState,
+    action: TransactionsViewAction,
+    environment: World
+) -> AnyPublisher<TransactionsViewAction, Never> {
+    switch action {
+    case .add(let transaction):
+        state.transactions.append(transaction)
+    case .edit(let transaction):
+        if let index = state.transactions.enumerated().first(where: { $0.element.id == transaction.id })?.offset {
+            state.transactions[index] = transaction
+        }
+    case .delete(let index):
+        if index < state.transactions.count {
+            state.transactions.remove(at: index)
+        }
+    }
+    return Empty().eraseToAnyPublisher()
+}
+
+extension TransactionsViewState {
+    static let sampleTransactions: [Transaction] = [
+        Transaction(
+            id: 1,
+            title: "March 15 Payroll",
+            category: .init(title: "Salary"),
+            amount: 10000,
+            wallet: Wallet(),
+            rawDateTime: "2021-03-05"
+        ),
+        Transaction(
+            id: 2,
+            title: "House Rental",
+            category: .init(title: "BILLS"),
+            amount: 5000,
+            wallet: Wallet(),
+            rawDateTime: "2021-03-05"
+        ),
+        Transaction(
+            id: 3,
+            title: "Sideline Payment",
+            category: .init(title: "Salary"),
+            amount: 10000,
+            wallet: Wallet(),
+            rawDateTime: "2021-03-05"
+        ),
+        Transaction(
+            id: 4,
+            title: "Sideline Payment",
+            category: .init(title: "Salary"),
+            amount: 100000,
+            wallet: Wallet(),
+            rawDateTime: "2021-03-05"
+        )
+    ]
+}
