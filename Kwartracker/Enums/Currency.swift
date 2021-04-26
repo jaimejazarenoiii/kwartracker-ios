3d68761b0f8c96d56a7b2136500bfa7750df2042
Jacob <jacob_potolin@ligph.com>
2021-04-26 09:25:43 +0800

    Create enum Currency and struct Category

---
+++ Kwartracker/Enums/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  Kwartracker
+//
+//  Created by Jacob on 4/26/21.
+//
+
+import Foundation
+
+enum Currency: String {
+    case philippinePeso = "fil_PH"
+    case usDollar = "en_US"
+}
