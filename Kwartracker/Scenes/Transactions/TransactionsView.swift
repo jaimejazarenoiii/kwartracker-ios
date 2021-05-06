ccf23b6857932586892644b0a82de43fc4c3d0ba
Jacob <jacob_potolin@ligph.com>
2021-05-06 20:55:09 +0800

    Use foreach inside list for the rowbackground to work

--- Kwartracker/Scenes/Transactions/TransactionsView.swift
+++ Kwartracker/Scenes/Transactions/TransactionsView.swift
@@ -111,8 +111,10 @@ struct TransactionsView: View {
                         Spacer()
                             .frame(height: 20)
 
-                        List(store.state.transactionState.transactions, id: \.id) {
-                            TransactionRowContent(transaction: $0)
+                        List {
+                            ForEach(store.state.transactionState.transactions, id: \.id) {
+                                TransactionRowContent(transaction: $0)
+                            }
                         }
                     }
                 }
