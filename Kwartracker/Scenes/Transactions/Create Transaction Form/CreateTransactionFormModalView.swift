226d5b6821a3c77c7644c0c24d3aaff50ac95270
Jacob <jacob_potolin@ligph.com>
2021-05-08 20:58:10 +0800

    Add the create transaction form modal

---
+++ Kwartracker/Scenes/Transactions/Create Transaction Form/CreateTransactionFormModalView.swift
@@ -0,0 +1,85 @@
+//
+//  CreateTransactionFormModalView.swift
+//  Kwartracker
+//
+//  Created by Jacob on 5/8/21.
+//
+
+import SwiftUI
+
+struct CreateTransactionFormModalView: View {
+    @EnvironmentObject var store: AppStore
+    @Binding var isPresented: Bool
+
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 30) {
+                Spacer()
+                    .frame(height: 10)
+                HStack(alignment: .center) {
+                    Text(L10n.TransactionsCreateFormModal.title)
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color(Asset.Colors.charcoal.color))
+                    Spacer()
+                    Button(action: {
+                        toggleModalWithAnimation()
+                    }, label: {
+                        Image(uiImage: Asset.Images.closeIcon.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 10, height: 10)
+                    })
+                    .buttonStyle(CircleButtonStyle(buttonColor: .white, padding: 10))
+                }
+                .padding([.leading, .trailing], 30)
+
+                VStack(spacing: 20) {
+                    SelectableFieldForm(
+                        label: L10n.TransactionsCreateFormModal.InputLabel.dateRange,
+                        selectLabel: L10n.TransactionsCreateFormModal.InputField.dateRange
+                    )
+                    SelectableFieldForm(
+                        label: L10n.TransactionsCreateFormModal.InputLabel.walletType,
+                        selectLabel: L10n.TransactionsCreateFormModal.InputField.walletType
+                    )
+                    SelectableFieldForm(
+                        label: L10n.TransactionsCreateFormModal.InputLabel.category,
+                        selectLabel: L10n.TransactionsCreateFormModal.InputField.category
+                    )
+                    Spacer()
+                        .frame(height: 10)
+                    Button(action: {}) {
+                        HStack {
+                            Spacer()
+                            Text(L10n.TransactionsCreateFormModal.ButtonTitle.apply)
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                    }
+                    .buttonStyle(RoundedRectangleButtonStyle(buttonColor: Asset.Colors.teal.color))
+                }
+                .padding([.leading, .trailing], 30)
+            }
+            .background(
+                Rectangle()
+                    .fill(Color(Asset.Colors.solitudeGrey.color))
+                    .cornerRadius(45, corners: [.topLeft, .topRight])
+                    .edgesIgnoringSafeArea(.bottom)
+            )
+            .offset(y: isPresented ? 0 : UIScreen.main.bounds.height)
+        }
+        .background(
+            Color.black
+                .opacity(isPresented ? 0.6 : 0)
+                .ignoresSafeArea()
+        )
+    }
+
+    func toggleModalWithAnimation() {
+        withAnimation {
+            isPresented.toggle()
+        }
+    }
+}
