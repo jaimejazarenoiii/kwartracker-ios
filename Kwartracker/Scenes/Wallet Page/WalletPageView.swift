85e6f11ee4f6ee1a614f0d53ca1fa7736e17b87a
Leah <ljylaya1996@gmail.com>
2021-05-17 16:56:34 +0800

    Implement KNavigationView to WalletPageView

--- Kwartracker/Scenes/Wallet Page/WalletPageView.swift
+++ Kwartracker/Scenes/Wallet Page/WalletPageView.swift
@@ -10,33 +10,48 @@ import SwiftUI
 struct WalletPageView: View {
     @EnvironmentObject var store: AppStore
     @State private var margin: CGFloat = 30
+    @State private var buttonToggle: Bool = false
     private var separator: UIColor = Asset.Colors.spindleGrey.color
     private var separatorHeight: CGFloat = 0.5
     var body: some View {
-        AuthLayoutView {
-            WalletNavigationView(navigationTitle: L10n.Wallet.Title.myWallet)
-        } content: {
-            ScrollView(showsIndicators: true) {
-                VStack {
-                    WalletOneCardCenterView(wallets: store.state.walletState.wallets)
-                    
-                    Spacer()
-                        .frame(height: margin)
-                    
-                    WalletSectionHeader()
-                    
-                    Spacer()
-                        .frame(height: margin)
-                    ForEach(store.state.transactionState.transactions, id: \.id) {
-                        TransactionRowContent(transaction: $0)
-                        Rectangle()
-                            .fill(Color(separator))
-                            .frame(height: separatorHeight)
-                    }
-                    .frame(width: UIScreen.main.bounds.width * 0.8)
+        NavigationView {
+            AuthLayoutView {
+                KNavigationView(L10n.Wallet.Title.myWallet,
+                                destination: AnyView(TransactionsView()),
+                                buttonToggle: $buttonToggle) {
+                    Button(action: {
+                        buttonToggle.toggle()
+                    }, label: {
+                        Image(uiImage: Asset.Images.addIconTeal.image)
+                            .frame(width: 10, height: 10)
+                    })
+                    .buttonStyle(CircleButtonStyle(buttonColor: .white))
                 }
-                .padding(.top, margin)
-            }.background(Color(Asset.Colors.solitudeGrey.color))
+            } content: {
+                ScrollView(showsIndicators: true) {
+                    VStack {
+                        WalletOneCardCenterView(wallets: store.state.walletState.wallets)
+                        
+                        Spacer()
+                            .frame(height: margin)
+                        
+                        WalletSectionHeader()
+                        
+                        Spacer()
+                            .frame(height: margin)
+                        ForEach(store.state.transactionState.transactions, id: \.id) {
+                            TransactionRowContent(transaction: $0)
+                            Rectangle()
+                                .fill(Color(separator))
+                                .frame(height: separatorHeight)
+                        }
+                        .frame(width: UIScreen.main.bounds.width * 0.8)
+                    }
+                    .padding(.top, margin)
+                }.background(Color(Asset.Colors.solitudeGrey.color))
+            }
+            
         }
+        .navigationBarHidden(true)
     }
 }
