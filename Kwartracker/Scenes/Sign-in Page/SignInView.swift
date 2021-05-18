b5962c4a1d4954825cc50f6a4e942b7712cbd14c
John Ellie Go <ellie.go12@gmail.com>
2021-05-18 14:29:33 +0800

    Skeletal view feedback fixes

--- Kwartracker/Scenes/Sign-in Page/SignInView.swift
+++ Kwartracker/Scenes/Sign-in Page/SignInView.swift
@@ -9,14 +9,12 @@ import SwiftUI
 
 struct SignInView: View {
     var body: some View {
-        AuthLayoutView {
+        SkeletalView(header: {
             SignInHeaderView()
-        } content: {
-            VStack(spacing: 0) {
-                SignInContentView()
-                SignInFooterView()
-            }
-        }
+        }, body: {
+            SignInContentView()
+            SignInFooterView()
+        })
     }
 }
 
