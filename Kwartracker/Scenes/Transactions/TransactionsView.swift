92d8b1a3cdb77d12a96222350d1fdc6ed0f11cc8
Jacob <jacob_potolin@ligph.com>
2021-05-31 11:54:29 +0800

    Create private Views and group as possible

--- Kwartracker/Scenes/Transactions/TransactionsView.swift
+++ Kwartracker/Scenes/Transactions/TransactionsView.swift
@@ -12,7 +12,7 @@ struct TransactionsView: View {
     @State var searchTransaction: String = ""
 
     init() {
-        setUpContentAppearance()
+        setUpTableViewAppearance()
     }
 
     var body: some View {
@@ -21,38 +21,7 @@ struct TransactionsView: View {
                 .ignoresSafeArea(.all)
 
             VStack {
-                HStack {
-                    Spacer()
-                        .frame(width: 30)
-
-                    Button(action: {
-                    }) {
-                        Image(uiImage: Asset.Images.arrowLeftIconWhite.image)
-                            .frame(width: 10, height: 10)
-                    }
-                    .buttonStyle(
-                        CircleButtonStyle(buttonColor: Asset.Colors.teal.color)
-                    )
-
-                    Spacer()
-
-                    Text(L10n.TransactionsPage.titleBar)
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .font(.title2)
-
-                    Spacer()
-
-                    Button(action: {
-                    }, label: {
-                        Image(uiImage: Asset.Images.addIconTeal.image)
-                            .frame(width: 10, height: 10)
-                    })
-                    .buttonStyle(CircleButtonStyle(buttonColor: .white))
-
-                    Spacer()
-                        .frame(width: 30)
-                }
+                TransactionsHeaderView()
 
                 Spacer()
                     .frame(height: 30)
@@ -64,68 +33,11 @@ struct TransactionsView: View {
                         .edgesIgnoringSafeArea(.bottom)
 
                     VStack {
-                        Spacer()
-                            .frame(height: 30)
-
-                        HStack {
-                            Spacer()
-                                .frame(width: 30)
-
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color(Asset.Colors.solitudeGrey.color))
-                                .frame(height: 48)
-                                .shadow(color: Color.black.opacity(0.2), radius: 15, x: 7, y: 7)
-                                .shadow(color: Color.white.opacity(0.7), radius: 15, x: -5, y: -5)
-                                .overlay(
-                                    HStack {
-                                        TextField(
-                                            L10n.TransactionsPage.transactionSearchPlaceholder,
-                                            text: $searchTransaction
-                                        )
-                                        .background(Color.clear)
-                                        .frame(height: 48, alignment: .center)
-                                        .padding(.leading, 20)
-                                        .keyboardType(.default)
-
-                                        Spacer()
-
-                                        Image(uiImage: Asset.Images.searchIcon.image)
-                                            .resizable()
-                                            .frame(width: 20, height: 20)
-                                            .padding(.trailing, 20)
-                                    }
-                                )
-
-                            Button(action: {
-                            }) {
-                                Image(uiImage: Asset.Images.filterIcon.image)
-                            }
-                            .buttonStyle(
-                                RoundedRectangleButtonStyle(buttonColor: Asset.Colors.teal.color)
-                            )
-
-                            Spacer()
-                                .frame(width: 30)
-                        }
-
-                        Spacer()
-                            .frame(height: 20)
-
-                        List {
-                            ForEach(store.state.transactionState.transactions, id: \.id) {
-                                TransactionRow(transaction: $0)
-                            }
-                            if store.state.transactionState.shouldShowLoadmore {
-                                HStack {
-                                    Spacer()
-                                    CircularLoaderView()
-                                        .frame(width: 30, height: 30)
-                                    Spacer()
-                                }
-                                .padding(.top, 10)
-                            }
-                        }
-                        .listStyle(InsetListStyle())
+                        TransactionsSearchBarView(searchTransaction: $searchTransaction)
+                        TransactionsListView(
+                            transactions: store.state.transactionState.transactions,
+                            shouldShowLoadmore: store.state.transactionState.shouldShowLoadmore
+                        )
                     }
                 }
             }
@@ -133,8 +45,134 @@ struct TransactionsView: View {
         }
     }
 
-    private func setUpContentAppearance() {
+    private func setUpTableViewAppearance() {
         UITableView.appearance().backgroundColor = .clear
         UITableViewCell.appearance().backgroundColor = .clear
+    }
+}
+
+private struct TransactionsHeaderView: View {
+    var body: some View {
+        HStack {
+            Spacer()
+                .frame(width: 30)
+
+            Button(action: {
+            }) {
+                Image(uiImage: Asset.Images.arrowLeftIconWhite.image)
+                    .frame(width: 10, height: 10)
+            }
+            .buttonStyle(
+                CircleButtonStyle(buttonColor: Asset.Colors.teal.color)
+            )
+
+            Spacer()
+
+            Text(L10n.TransactionsPage.titleBar)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .font(.title2)
+
+            Spacer()
+
+            Button(action: {
+            }, label: {
+                Image(uiImage: Asset.Images.addIconTeal.image)
+                    .frame(width: 10, height: 10)
+            })
+            .buttonStyle(CircleButtonStyle(buttonColor: .white))
+
+            Spacer()
+                .frame(width: 30)
+        }
+    }
+}
+
+private struct TransactionsSearchBarView: View {
+    @Binding var searchTransaction: String
+
+    var body: some View {
+        Group {
+            Spacer()
+                .frame(height: 30)
+
+            HStack {
+                Spacer()
+                    .frame(width: 30)
+
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color(Asset.Colors.solitudeGrey.color))
+                    .frame(height: 48)
+                    .shadow(color: Color.black.opacity(0.2), radius: 15, x: 7, y: 7)
+                    .shadow(color: Color.white.opacity(0.7), radius: 15, x: -5, y: -5)
+                    .overlay(
+                        HStack {
+                            TextField(
+                                L10n.TransactionsPage.transactionSearchPlaceholder,
+                                text: $searchTransaction
+                            )
+                            .background(Color.clear)
+                            .frame(height: 48, alignment: .center)
+                            .padding(.leading, 20)
+                            .keyboardType(.default)
+
+                            Spacer()
+
+                            Image(uiImage: Asset.Images.searchIcon.image)
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .padding(.trailing, 20)
+                        }
+                    )
+
+                Button(action: {
+                }) {
+                    Image(uiImage: Asset.Images.filterIcon.image)
+                }
+                .buttonStyle(
+                    RoundedRectangleButtonStyle(buttonColor: Asset.Colors.teal.color)
+                )
+
+                Spacer()
+                    .frame(width: 30)
+            }
+        }
+    }
+}
+
+private struct TransactionsListView: View {
+    var transactions: [Transaction]
+    var shouldShowLoadmore: Bool = false
+
+    var body: some View {
+        Group {
+            Spacer()
+                .frame(height: 20)
+
+            List {
+                ForEach(transactions, id: \.id) {
+                    TransactionRow(transaction: $0)
+                }
+                LoaderView(shouldShowLoadmore: shouldShowLoadmore)
+            }
+            .listStyle(InsetListStyle())
+        }
+    }
+}
+
+private struct LoaderView: View {
+    var shouldShowLoadmore: Bool
+    var body: some View {
+        Group {
+            if shouldShowLoadmore {
+                HStack {
+                    Spacer()
+                    CircularLoaderView()
+                        .frame(width: 30, height: 30)
+                    Spacer()
+                }
+                .padding(.top, 10)
+            }
+        }
     }
 }
