2dc9807aa16cbbe5c9f419ed27ca11a6d15ff505
John Ellie Go <ellie.go12@gmail.com>
2021-06-16 14:18:49 +0800

    Sign-up view model

---
+++ Kwartracker/Scenes/SignUp/Sign-up View-Model/SignUpViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewModel.swift
+//  Kwartracker
+//
+//  Created by John Ellie Go on 6/15/21.
+//
+
+import SwiftUI
+import Apollo
+
+final class SignUpViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var isAuthenticated: Bool = false
+    
+    var userInfo: SignInMutation.Data.SignInWithEmail?
+    
+    func doRegisterUser() {
+        let loginCredentials = CredentialsInput(email: email, password: password)
+        let mutation = SignInMutation(signInCredential: loginCredentials)
+        
+        Network.shared.apollo.perform(mutation: mutation) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let graphQLResult):
+                if let data = graphQLResult.data?.signInWithEmail {
+                    self.userInfo = data
+                    self.isAuthenticated = true
+                }
+            case .failure(let error):
+                print("Failure! Error: \(error)")
+            }
+        }
+    }
+}
