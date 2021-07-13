0373859bfc8a04a1bb19104304816b7279e22958
Jacob <jake.braul@gmail.com>
2021-07-13 22:14:07 +0800

    Update the add category form

--- Kwartracker/Scenes/Category Page/CategoryView.swift
+++ Kwartracker/Scenes/Category Page/CategoryView.swift
@@ -8,20 +8,55 @@
 import SwiftUI
 
 struct CategoryView: View {
+    @EnvironmentObject private var store: AppStore
     var body: some View {
-        SkeletalView(header: {
-            NavigationHeaderView(
-                navigationTitle: L10n.SettingsPage.NavigationItem.categories,
-                navigationButton: ""
-            )
-        }, body: {
-            CategoryBodyView()
-        })
+        NavigationView {
+            SkeletalView(header: {
+                CategoryHeaderView().padding(.top, 10)
+                    .environmentObject(store)
+            }, body: {
+                CategoryBodyView().environmentObject(store)
+            })
+            .navigationBarHidden(true)
+        }
     }
 }
 
 struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
         CategoryView()
+    }
+}
+
+private struct CategoryHeaderView: View {
+    @EnvironmentObject private var store: AppStore
+    @State private var isAddCategoryLinkActive: Bool = false
+    private let imageSize: CGSize = CGSize(width: 10, height: 10)
+
+    var body: some View {
+        NavigationBarView(
+            title: L10n.SettingsPage.NavigationItem.categories) {
+            Button(action: {}) {
+                Image(uiImage: Asset.Images.arrowLeftIconWhite.image)
+                    .frame(width: 10, height: 10)
+            }
+            .buttonStyle(
+                CircleButtonStyle(buttonColor: Asset.Colors.teal.color)
+            )
+        } rightBarViewContent: {
+            NavigationLink(
+                destination: EditCategoryView(categoryName: "Add Category")
+                    .environmentObject(store),
+                isActive: $isAddCategoryLinkActive
+            ) {
+                Button(action: {
+                    isAddCategoryLinkActive.toggle()
+                }) {
+                    Image(uiImage: Asset.Images.addIconTeal.image)
+                        .frame(width: imageSize.width, height: imageSize.height)
+                }
+                .buttonStyle(CircleButtonStyle(buttonColor: .white))
+            }
+        }
     }
 }
