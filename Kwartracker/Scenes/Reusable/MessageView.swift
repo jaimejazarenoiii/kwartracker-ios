be64cb948a101e7eac9c3022c897e32093b6301e
Jacob <jake.braul@gmail.com>
2021-08-23 17:09:52 +0800

    Modify alert view content and its messages

--- Kwartracker/Scenes/Reusable/MessageView.swift
+++ Kwartracker/Scenes/Reusable/MessageView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct MessageView: View {
-    
-    let state: DialogStatus
+    let topImage: UIImage
+    let title: String
+    let message: String
     
     var body: some View {
         VStack {
-            Image(uiImage: state.image)
-            Text(state.title).foregroundColor(Color(Asset.Colors.nightRider.color))
+            Image(uiImage: topImage)
+            Text(title).foregroundColor(Color(Asset.Colors.nightRider.color))
                 .font(.system(size: 20))
                 .fontWeight(.medium)
                 .frame(width: 200, height: 30, alignment: .center)
                 .fixedSize(horizontal: false, vertical: true)
-            Text(state.message).foregroundColor(Color(Asset.Colors.spindleGrey.color))
+            Text(message).foregroundColor(Color(Asset.Colors.spindleGrey.color))
                 .frame(width: 220, height: 30, alignment: .center)
                 .font(.system(size: 10))
                 .fixedSize(horizontal: false, vertical: true)
