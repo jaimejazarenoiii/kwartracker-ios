794096a6eb6071a1b23a8477e2d3a322f05926f8
Jacob <jake.braul@gmail.com>
2021-08-27 11:31:00 +0800

    Make topImage to accept nil, make message font bigger, and set max width and max height

--- Kwartracker/Scenes/Reusable/MessageView.swift
+++ Kwartracker/Scenes/Reusable/MessageView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct MessageView: View {
-    let topImage: UIImage
+    let topImage: UIImage?
     let title: String
     let message: String
     
     var body: some View {
         VStack {
-            Image(uiImage: topImage)
+            if let topImage = topImage {
+                Image(uiImage: topImage)
+            }
             Text(title).foregroundColor(Color(Asset.Colors.nightRider.color))
                 .font(.system(size: 20))
                 .fontWeight(.medium)
                 .frame(width: 200, height: 30, alignment: .center)
                 .fixedSize(horizontal: false, vertical: true)
             Text(message).foregroundColor(Color(Asset.Colors.spindleGrey.color))
-                .frame(width: 220, height: 30, alignment: .center)
-                .font(.system(size: 10))
+                .frame(maxWidth: 220, minHeight: 30, maxHeight: 400, alignment: .center)
+                .font(.system(size: 17))
                 .fixedSize(horizontal: false, vertical: true)
         }
     }
