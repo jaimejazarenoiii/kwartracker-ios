31ecb8123cb9c5987007e128254a3f94385a5605
Jacob <jake.braul@gmail.com>
2021-08-31 21:34:07 +0800

    Add redirection to login and security page

--- Kwartracker/Scenes/Login and Security Page/LoginAndSecurityView.swift
+++ Kwartracker/Scenes/Login and Security Page/LoginAndSecurityView.swift
@@ -8,20 +8,43 @@
 import SwiftUI
 
 struct LoginAndSecurityView: View {
+    var navigationBackAction: (() -> ())
+    @State private var isChangePasswordLinkActive = false
     var body: some View {
-        SkeletalView(header: {
-            NavigationHeaderView(
-                navigationTitle: L10n.SettingsPage.NavigationItem.loginAndSecurity,
-                navigationButton: ""
-            )
-        }, body: {
-            LoginAndSecuritBodyView()
-        })
+        ZStack {
+            SkeletalView(
+                header: { header },
+                body: {
+                    LoginAndSecuritBodyView(isChangePasswordLinkActive: $isChangePasswordLinkActive)
+            })
+            .navigationBarHidden(true)
+
+            NavigationLink(
+                destination: ChangePasswordView(
+                    navigationBackAction: { isChangePasswordLinkActive = false }),
+                isActive: $isChangePasswordLinkActive) {
+                EmptyView()
+            }
+        }
+    }
+
+    private var header: some View {
+        NavigationBarView(
+            title: L10n.SettingsPage.NavigationItem.loginAndSecurity,
+            leftBarViewContent: {
+                Button(action: navigationBackAction) {
+                    Image(uiImage: Asset.Images.arrowLeftIconWhite.image)
+                        .frame(width: 10, height: 10)
+                }
+                .buttonStyle(CircleButtonStyle(buttonColor: Asset.Colors.teal.color))
+            },
+            rightBarViewContent: { EmptyView() }
+        )
     }
 }
 
 struct LoginAndSecurityView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginAndSecurityView()
+        LoginAndSecurityView(navigationBackAction: {})
     }
 }
