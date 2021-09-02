a874d79ca811958d015c7f6fbc51402907780ff0
Jacob <jake.braul@gmail.com>
2021-09-02 09:50:50 +0800

    Use NavigationBarLayout in EditProfileView

--- Kwartracker/Scenes/Edit-Profile Page/EditProfileView.swift
+++ Kwartracker/Scenes/Edit-Profile Page/EditProfileView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct EditProfileView: View {
+    var navigationBackAction: (() -> ())
     var body: some View {
-        SkeletalView(header: {
-            NavigationHeaderView(
-                navigationTitle: L10n.EditProfilePage.navigationTitle,
-                navigationButton: L10n.EditProfilePage.NavigationButtonItem.save
-            )
+        NavigationBarLayout(header: {
+            NavigationBarView(title: L10n.EditProfilePage.navigationTitle) {
+                Button(action: navigationBackAction) {
+                    Image(uiImage: Asset.Images.arrowLeftIconWhite.image)
+                        .frame(width: 10, height: 10)
+                }
+                .buttonStyle(
+                    CircleButtonStyle(buttonColor: Asset.Colors.teal.color)
+                )
+            } rightBarViewContent: {
+                Button(action: saveProfileAction, label: {
+                    Text(L10n.EditProfilePage.NavigationButtonItem.save)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                })
+                .frame(width: 40, alignment: .center)
+            }
         }, body: {
             EditProfileContentView()
         })
+        .navigationBarHidden(true)
+    }
+
+    private func saveProfileAction() {
     }
 }
 
 struct EditProfileVIew_Previews: PreviewProvider {
     static var previews: some View {
-        EditProfileView()
+        EditProfileView(navigationBackAction: {})
     }
 }
